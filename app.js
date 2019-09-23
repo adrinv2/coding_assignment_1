310a5fabc49f4ac1f2a4a410dc39f535e784b8e0
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2019-09-23 12:10:38 +0200

    added html

--- app.js
+++ app.js
@@ -5,16 +5,71 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 
 //Storing port number and our full app
-var port = 3000;
+var port = 8081;
 var app = express();
 
 //STEP 1: Setting up the boilerplate and routing
-app.get('/', function(req, res){
+app.get('/wikipedia', function(req, res){
 
+  var url = 'https://en.wikipedia.org/wiki/Quartz';
+
+  request(url, function(error, response, html) {
+    if(!error) {
+      // res.send(html);
+      var $ = cheerio.load(html);
+      var data = {
+        articleTitle:'',
+        articleImage: '',
+        articleParagraph: ''
+      }
+
+      $('#content').filter(function(){
+        data.articleTitle = $(this).find('#firstHeading').text();
+        data.articleImage = $(this).find('img').attr('src');
+        data.articleParagraph = $(this).find('p:nth-of-type(2)').first().text();
+      });
+
+      res.send(data);
+
+      fs.writeFile('wiki-output.js', JSON.stringify(data, null, 4), function(error){
+        console.log('File written on hard drive!');
+
+      });
+    }
+  });
 //All the web scraping magic will happen here
-  res.send('Hello World!');
+//  res.send('Hello World!');
 
 });
+
+app.get('/imdb', function(req, res){
+
+  var url = 'https://www.imdb.com/chart/top';
+
+  request(url, function(error, response, html) {
+    if(!error) {
+      // res.send(html);
+      var $ = cheerio.load(html);
+      var data = [];
+
+      $('.lister-list').filter(function(){
+        $(this).find('tr').each(function(i, elem){
+        data[i] = "'" + $(this). find('.posterColumn').find('img').attr('src') + "'"
+      });
+});
+      res.send(data);
+
+      fs.writeFile('imdb-output.js', 'var imdb_list = [' + data +']', function(error){
+        console.log('File written on hard drive!');
+
+      });
+    }
+  });
+//All the web scraping magic will happen here
+//  res.send('Hello World!');
+
+});
+
 
 app.listen(port);
 console.log('Magic happens on port' + port);
