2fa7763c058e5ea2da25a6476c340ee3c49a6fff
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2019-10-28 13:23:01 +0100

    scrapping + edits

--- app.js
+++ app.js
@@ -13,7 +13,69 @@ var url = require('url');
 var port = 8081;
 var app = express();
 
-var DOWNLOAD_DIR = 'C:/Users/adria/Documents/GitHub/coding_assignment_1/';
+var DOWNLOAD_DIR = 'C:/Users/adria/Desktop/GitHub/coding_assignment_1';
+
+// INSTAGRAM SCRAPER: access by going to 'localhost:2100/instagram'
+app.get('/instagram', function(req, res){
+
+  // try any hashtags and see the results, make sure to write INSIDE the quotation marks
+  var hashtag = 'custodiadeArfe';
+  var url = 'https://instagram.com/explore/tags/'+ hashtag +'/?__a=1';
+
+  // let's make the http request to the url above using the 'request' dependency
+  request(url, function(error, response, html) {
+
+    // only execute if there's no error
+    if(!error) {
+
+      // we can use the dependency 'cheerio' to traverse the DOM and use jQuery-like selectors and functions
+      var $ = cheerio.load(html);
+
+      // the url actually gives back already a ready to use JSON object so we just want that raw text
+      var instagram_data = JSON.parse($.text());
+      var instagram_urls = [];
+
+      for(var i = 0; i < instagram_data.graphql.hashtag.edge_hashtag_to_media.edges.length; i++) {
+        instagram_urls[i] = instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url;
+
+        download_file_curl(instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
+
+        // fs.createWriteStream('./data/'+[i]+'.jpg', instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url, function(err){
+        //   console.log('File is written successfully!');
+        // });
+      }
+
+      // send the data we've stored in our array back to the browser
+      res.send(instagram_urls);
+
+      // save the data we've stored in our object on our machine
+
+    }
+  });
+});
+
+var download_file_curl = function(file_url) {
+  // extract the file name
+  var file_name = url.parse(file_url).pathname.split('/').pop();
+  // create an instance of writable stream
+  var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+  // execute curl using child_process' spawn function
+  var curl = spawn('curl', [file_url]);
+  // add a 'data' event listener for the spawn instance
+  curl.stdout.on('data', function(data) { file.write(data); });
+  // add an 'end' event listener to close the writeable stream
+  curl.stdout.on('end', function(data) {
+    file.end();
+    console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+  });
+  // when the spawn child process exits, check if there were any errors and close the writeable stream
+  curl.on('exit', function(code) {
+    if (code != 0) {
+      console.log('Failed: ' + code);
+    }
+  });
+};
+
 
 // //STEP 1: Setting up the boilerplate and routing
 // app.get('/wikipedia', function(req, res){
@@ -49,59 +111,59 @@ var DOWNLOAD_DIR = 'C:/Users/adria/Documents/GitHub/coding_assignment_1/';
 //
 // });
 
-app.get('/google', function(req, res){
-
-  var url = 'https://www.google.com/search?rlz=1C1CHBD_csCZ736CZ736&biw=865&bih=610&tbm=isch&sxsrf=ACYBGNR3vKf4Pxtg1ZVX-UgUNg05pR2ntA%3A1570977896409&sa=1&ei=aDijXdfYGKyJk74PxbeGgAE&q=el+expolio+el+greco&oq=el+expolio+el+greco&gs_l=img.3...0.0..4919902...0.0..0.0.0.......0......gws-wiz-img.e9ESRPMR1lE&ved=0ahUKEwjXrNzbvJnlAhWsxMQBHcWbARAQ4dUDCAc&uact=5';
-
-  request(url, function(error, response, html) {
-    if(!error) {
-      // res.send(html);
-      var $ = cheerio.load(html);
-      var google_data = [];
-
-      // $(this).find("img").each(function(){
-      //   google_data.push($(this).attr('src'));
-      // });
-
-      $('#ires').filter(function(){
-        $(this).find('td').each(function(i, elem){
-        google_data[i] = "'" + $(this).find('img').attr('src') + "'"
-        download_file_curl($(this).find('img').attr('src'));
-      });
-});
-      res.send(google_data);
-
-      fs.writeFile('google_output.js',"var google_output = [" + google_data + "]", function(error){
-              console.log("File is written successfully!");
-
-      });
-    }
-  });
-//All the web scraping magic will happen here
-//  res.send('Hello World!');
-
-});
-var download_file_curl = function(file_url) {
-  // extract the file name
-  var file_name = url.parse(file_url).pathname.split('/').pop();
-  // create an instance of writable stream
-  var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
-  // execute curl using child_process' spawn function
-  var curl = spawn('curl', [file_url]);
-  // add a 'data' event listener for the spawn instance
-  curl.stdout.on('data', function(data) { file.write(data); });
-  // add an 'end' event listener to close the writeable stream
-  curl.stdout.on('end', function(data) {
-    file.end();
-    console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-  });
-  // when the spawn child process exits, check if there were any errors and close the writeable stream
-  curl.on('exit', function(code) {
-    if (code != 0) {
-      console.log('Failed: ' + code);
-    }
-  });
-};
+// app.get('/google', function(req, res){
+//
+//   var url = 'https://www.google.com/search?rlz=1C1CHBD_csCZ736CZ736&biw=865&bih=610&tbm=isch&sxsrf=ACYBGNR3vKf4Pxtg1ZVX-UgUNg05pR2ntA%3A1570977896409&sa=1&ei=aDijXdfYGKyJk74PxbeGgAE&q=el+expolio+el+greco&oq=el+expolio+el+greco&gs_l=img.3...0.0..4919902...0.0..0.0.0.......0......gws-wiz-img.e9ESRPMR1lE&ved=0ahUKEwjXrNzbvJnlAhWsxMQBHcWbARAQ4dUDCAc&uact=5';
+//
+//   request(url, function(error, response, html) {
+//     if(!error) {
+//       // res.send(html);
+//       var $ = cheerio.load(html);
+//       var google_data = [];
+//
+//       // $(this).find("img").each(function(){
+//       //   google_data.push($(this).attr('src'));
+//       // });
+//
+//       $('#ires').filter(function(){
+//         $(this).find('td').each(function(i, elem){
+//         google_data[i] = "'" + $(this).find('img').attr('src') + "'"
+//         download_file_curl($(this).find('img').attr('src'));
+//       });
+// });
+//       res.send(google_data);
+//
+//       fs.writeFile('google_output.js',"var google_output = [" + google_data + "]", function(error){
+//               console.log("File is written successfully!");
+//
+//       });
+//     }
+//   });
+// //All the web scraping magic will happen here
+// //  res.send('Hello World!');
+//
+// });
+// var download_file_curl = function(file_url) {
+//   // extract the file name
+//   var file_name = url.parse(file_url).pathname.split('/').pop();
+//   // create an instance of writable stream
+//   var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+//   // execute curl using child_process' spawn function
+//   var curl = spawn('curl', [file_url]);
+//   // add a 'data' event listener for the spawn instance
+//   curl.stdout.on('data', function(data) { file.write(data); });
+//   // add an 'end' event listener to close the writeable stream
+//   curl.stdout.on('end', function(data) {
+//     file.end();
+//     console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+//   });
+//   // when the spawn child process exits, check if there were any errors and close the writeable stream
+//   curl.on('exit', function(code) {
+//     if (code != 0) {
+//       console.log('Failed: ' + code);
+//     }
+//   });
+// };
 
 
 // app.get('/google', function(req, res){
@@ -131,67 +193,6 @@ var download_file_curl = function(file_url) {
 // //  res.send('Hello World!');
 //
 // });
-
-// // INSTAGRAM SCRAPER: access by going to 'localhost:2100/instagram'
-// app.get('/instagram', function(req, res){
-//
-//   // try any hashtags and see the results, make sure to write INSIDE the quotation marks
-//   var hashtag = 'domenikostheotokopoulos';
-//   var url = 'https://instagram.com/explore/tags/'+ hashtag +'/?__a=1';
-//
-//   // let's make the http request to the url above using the 'request' dependency
-//   request(url, function(error, response, html) {
-//
-//     // only execute if there's no error
-//     if(!error) {
-//
-//       // we can use the dependency 'cheerio' to traverse the DOM and use jQuery-like selectors and functions
-//       var $ = cheerio.load(html);
-//
-//       // the url actually gives back already a ready to use JSON object so we just want that raw text
-//       var instagram_data = JSON.parse($.text());
-//       var instagram_urls = [];
-//
-//       for(var i = 0; i < instagram_data.graphql.hashtag.edge_hashtag_to_media.edges.length; i++) {
-//         instagram_urls[i] = instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url;
-//
-//         download_file_curl(instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
-//
-//         // fs.createWriteStream('./data/'+[i]+'.jpg', instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url, function(err){
-//         //   console.log('File is written successfully!');
-//         // });
-//       }
-//
-//       // send the data we've stored in our array back to the browser
-//       res.send(instagram_urls);
-//
-//       // save the data we've stored in our object on our machine
-//
-//     }
-//   });
-// });
-//
-// var download_file_curl = function(file_url) {
-//   // extract the file name
-//   var file_name = url.parse(file_url).pathname.split('/').pop();
-//   // create an instance of writable stream
-//   var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
-//   // execute curl using child_process' spawn function
-//   var curl = spawn('curl', [file_url]);
-//   // add a 'data' event listener for the spawn instance
-//   curl.stdout.on('data', function(data) { file.write(data); });
-//   // add an 'end' event listener to close the writeable stream
-//   curl.stdout.on('end', function(data) {
-//     file.end();
-//     console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-//   });
-//   // when the spawn child process exits, check if there were any errors and close the writeable stream
-//   curl.on('exit', function(code) {
-//     if (code != 0) {
-//       console.log('Failed: ' + code);
-//     }
-//   });
-// };
 
 
 
